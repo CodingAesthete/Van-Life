78fcca77b583051bcf4a8d06961ec593f119dcca
CodingAesthete <harry_olli@abv.bg>
2023-12-21 18:26:00 +0200

    Minnor Fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
   return (
     <div className="home-container">
-      <img src={bgImg} className="about-hero-image" />
+      <img src={bgImg} className="about-hero-image" alt="pic" />
       <h1>You got the travel plans, we got the travel vans.</h1>
       <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
       <Link to="vans">Find your van</Link>
