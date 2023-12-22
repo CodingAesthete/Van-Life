72d5a1cf98f44cde435a047fc48bb2d9e375d60d
CodingAesthete <harry_olli@abv.bg>
2023-12-22 16:04:24 +0200

    Add margin-bottom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <img src={bgImg} className="about-hero-image" alt="pic" />
       <h1>You got the travel plans, we got the travel vans.</h1>
       <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-      <Link to="vans">Find your van</Link>
+      <Link to="/vans">Find your van</Link>
     </div>
   )
 };
