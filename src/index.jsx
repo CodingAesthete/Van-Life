f492fde08b65f3d5563e2d377743de921aa97ad5
CodingAesthete <harry_olli@abv.bg>
2023-12-26 19:57:39 +0200

    Add Host Pages

--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,8 @@ import Layout from './components/Layout';
 import HostLayout from './components/HostLayout';
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
+import HostVans from './pages/Host/HostVans';
+import HostVanDetail from './pages/Host/HostVanDetail';
 import Reviews from "./pages/Host/Reviews"
 import "./index.css"
 import "./server"
@@ -26,6 +28,8 @@ function App() {
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
 
